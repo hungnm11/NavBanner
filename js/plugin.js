5adc7fa67f868001561d8f584a5db1daec1e9294
hungnm11 <hung.nguyen@hotmail.com.vn>
2015-10-14 00:08:58 +0700

    update fonts

--- js/plugin.js
+++ js/plugin.js
@@ -10,6 +10,22 @@
 		
 		return this.each(function() {
 			
+//			$(".list-group-item").on("mouseover", function() {
+//				var $li = $(this);
+//				$li.find('a').addClass('active');
+//				$li.find('.nav-sub').show();
+//			});
+//			$(".list-group-item").on("mouseout", function() {
+//				var $li = $(this);
+//				$(".list-group-item a").removeClass('active');
+//				$(".list-group-item .nav-sub").hide();
+//			});
+			
+			expendPopup();
+		
+		});
+		
+		function expendPopup() {
 			$(".list-group-item").on("mouseover", function() {
 				var $li = $(this);
 				$li.find('a').addClass('active');
@@ -20,8 +36,6 @@
 				$(".list-group-item a").removeClass('active');
 				$(".list-group-item .nav-sub").hide();
 			});
-			
-		
-		});
+		}
 	}
 })(jQuery);
