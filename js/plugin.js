d7f769ba4b0950c47a3b3454d841d69908d94591
Hung Nguyen <hung.nguyen@hotmail.com.vn>
2015-10-14 14:56:04 +0700

    update banner

--- js/plugin.js
+++ js/plugin.js
@@ -2,30 +2,52 @@
 	$.fn.tiki = function(options) {
 		
 		var settings = $.extend({
-			text: "Hello World",
-			color: null,
-			fontStyle: null,
-			complete: null
+			
 		}, options);
 		
 		return this.each(function() {
-			
-//			$(".list-group-item").on("mouseover", function() {
-//				var $li = $(this);
-//				$li.find('a').addClass('active');
-//				$li.find('.nav-sub').show();
-//			});
-//			$(".list-group-item").on("mouseout", function() {
-//				var $li = $(this);
-//				$(".list-group-item a").removeClass('active');
-//				$(".list-group-item .nav-sub").hide();
-//			});
-			
-			expendPopup();
+			var y = 0;
+            var containerPhoto = $(".swiper-container-vertical");
+            var items = containerPhoto.find(".swiper-slide");
+            var containerThumbnail = $(".swiper-container");
+            var itemsThumb = containerThumbnail.find(".swiper-slide");
+            var containerHeight = 0;
+            var numberVisible = $(".swiper-container-vertical > .swiper-wrapper > *").length;
+            var heightPhoto = $(".swiper-container-vertical .swiper-slide img").height();
+            var interValSecs = 1000;
+            
+            exSubMenu();
+
+            function myFunc() {
+                console.log("hello");
+            }
+            
+            
+            var init = setInterval(myFunc, interValSecs);
+            
+            
+            
+            
+            itemsThumb.on({
+                mouseenter: function() {
+                    var imgPos = - ($(this).index() * heightPhoto) ;
+                    console.log(imgPos);
+                 
+                    containerPhoto.find(".swiper-wrapper").css({
+                        top: imgPos + "px",
+                    });
+                    clearInterval(init);
+                },
+                mouseleave: function() {
+                    init = setInterval(myFunc, interValSecs);
+                }
+                
+            });
 		
 		});
+        
 		
-		function expendPopup() {
+		function exSubMenu() {
 			$(".list-group-item").on("mouseover", function() {
 				var $li = $(this);
 				$li.find('a:first-child').addClass('active');
@@ -37,5 +59,6 @@
 				$(".list-group-item .nav-sub").hide();
 			});
 		}
+        
 	}
 })(jQuery);
